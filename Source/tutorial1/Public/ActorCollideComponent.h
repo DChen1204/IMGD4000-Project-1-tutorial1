2c8653e41d52aadae6ab2b3aa4972513c7810461
Diwen Chen <dchen5@wpi.edu>
2025-12-08 16:22:58 -0500

    Initial commit

---
+++ Source/tutorial1/Public/ActorCollideComponent.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ActorCollideComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TUTORIAL1_API UActorCollideComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UActorCollideComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
